a1a418010d254328411c8cc086bfc11e896fa42a
Álisson Braga C <alisson.b.c10@gmail.com>
2025-06-10 17:28:02 -0300

    Move requisicao da api para um backend no Render para manter a chave privada

--- html/js/apiGemini.js
+++ html/js/apiGemini.js
@@ -1,37 +1,25 @@
-async function gerarTexto(prompt, apiKey) {
-    const url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=" + apiKey;
-
-    const requestBody = {
-        contents: [{ parts: [{ text: prompt }] }],
-    };
-
-    const response = await fetch(url, {
-        method: "POST",
-        headers: {
-        "Content-Type": "application/json",
-        },
-        body: JSON.stringify(requestBody),
-    });
-
-    if (!response.ok) {
-        throw new Error(`Erro na requisição: ${response.status}`);
-    }
-
-    const data = await response.json();
-    return data.candidates[0].content.parts[0].text;
-}
-
 async function executarGeracao(tituloFilme) {
-    const apiKey = "AIzaSyCEtxAo0z5gq8AtMEbc7HRXpX5ictf1HbE"; // desisti de esconder a chave no backend, tenho pouco tempo esse final de semstre
     const prompt = `Gere uma sinopse do filme \"${tituloFilme}\" de forma resumida, sem muitos spoilers em até 200 palavras`;
 
     try {
-        const resultado = await gerarTexto(prompt, apiKey);
-        return resultado;
+        const resposta = await fetch("https://backendt4web.onrender.com", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ prompt })
+        });
+
+        if (!resposta.ok) {
+            throw new Error("Erro ao chamar o backend");
+        }
+
+        const dados = await resposta.json();
+        return dados.candidates[0].content.parts[0].text;
     } catch (erro) {
         console.error("Erro ao gerar texto:", erro);
-        return "Falha ao gerar sinopse com o Gemini."
+        return "Falha ao gerar sinopse com o Gemini.";
     }
 }
 
-export {executarGeracao };
+export { executarGeracao };
